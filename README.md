d1ad023a700801b03535363d5cd5c04c72d9de15
Vincent Oord <vincent@vincentoord.nl>
2017-10-10 16:34:38 +0200

    Update calculator with new calculations

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # Ergo Score Calculator
 
-Simple Javascript calculator to compare times and or distances rowed on a Concept 2 ergometer with other club members, taking age, weight and sex into account for a more fair ranking.
+Simple Javascript calculator to compare times and or distances rowed on a
+Concept 2 ergometer with other club members, taking age, weight and sex into
+account for a more fair ranking.
+
+This calculator was made to easily calculate the score for participants to the
+internal Ergometer Challenge of my rowing club, [KAR&ZV De Hoop][1], but feel
+free to use it for your own Ergometer Challenges too!
+
+## Version 1 (2016)
+The first edition the Challenge consisted of rowing 5k on a Concept 2 ergometer
+and getting the best possible split. We used the following format to determine
+the final score compensated for gender, weight and age.
+
+* Row fasted possible 5k and take your average split
+* Transform the average split to an output of watts / kg
+* Compensate for decreased performance per year over the age of 30 by 0,8%
+* Use a 15% multiplier for female participants
+
+## Version 2 (2017)
+In the section edition of the competition we changed some rules as both older
+and female members felt the compensation score was not totally fair. This time
+we used a modified format to make the end result more fair.
+
+* Row as many meters as possible in 20 minutes
+* Compensate that distance for your weight
+* Compensate for decreased performance per year over the age of 30 by 0,65%
+* Use a 20% multiplier for female participants
+
+
+[1]: http://karzvdehoop.nl
---
+++ js/ergo-score-calculator.js
@@ -0,0 +1,97 @@
+var ergoScoreCalculator = (function() {
+  var calculatorForm,
+    scoreContainer;
+
+  function processCalculatorForm(event) {
+    event.preventDefault();
+
+    var distance = document.querySelector('[name=distance]').value;
+    var weight = document.querySelector('[name=weight]').value;
+    var age = document.querySelector('[name=age]').value;
+    var gender = document.querySelector('[name=gender]:checked').value;
+    var formError = validateForm(distance, weight, age, gender);
+
+    if(formError) {
+      alert(formError);
+    } else {
+      calculateScore(distance, weight, age, gender);
+    }
+  }
+
+  function validateForm(distance, weight, age, gender) {
+    var error = null;
+    if (distance === '' || weight === '' || age === '' || gender === '') {
+      error = 'Alle velden moeten worden ingevuld';
+    }
+    return error;
+  }
+
+  function genderMultiplier(gender) {
+    return gender === 'f' ? 120 : 100;
+  }
+
+  function weightFactor(weightInLbs) {
+    return Math.pow(weightInLbs / 270, 0.222);
+  }
+
+  function kgToLbs(weightInKg) {
+    return weightInKg * 2.2046;
+  }
+
+  function parseWeightInput(weight) {
+    return parseFloat(weight.replace(',', '.'));
+  }
+
+  function compensateDistance(distance, weightInLbs) {
+    return distance / weightFactor(weightInLbs);
+  }
+
+  function parseAgeFactor(age) {
+    ageDifference = parseInt(age) > 30 ? age - 30 : 0;
+    factor = 100 * Math.pow(1 - 0.0065, ageDifference);
+    return parseFloat(factor).toFixed(1);
+  }
+
+  function calculateScore(distance, weight, age, gender) {
+    var ageFactor = parseAgeFactor(age);
+    var parsedWeight = kgToLbs(parseWeightInput(weight));
+    var compensatedDistance = compensateDistance(distance, parsedWeight);
+    var finalScore = compensatedDistance / ageFactor * genderMultiplier(gender);
+
+    scoreContainer.innerText = parseFloat(finalScore).toFixed(3);
+    scoreContainer.scrollIntoView();
+
+    console.log({
+      score: finalScore,
+      distance: {
+        input: distance,
+        compensated: compensatedDistance
+      },
+      weight: {
+        input: weight,
+        parsed: parseWeightInput(weight),
+        lbs: parsedWeight,
+        factor: weightFactor(parsedWeight)
+      },
+      age: {
+        input: age,
+        factor: ageFactor
+      },
+      gender: {
+        input: gender,
+        multiplier: genderMultiplier(gender)
+      }
+    });
+  };
+
+  var init = function() {
+    calculatorForm = document.querySelector('#ergoCalculator');
+    scoreContainer = document.querySelector('#score');
+
+    calculatorForm.addEventListener('submit', processCalculatorForm, false);
+  };
+
+  return {
+    init: init
+  };
+}());
